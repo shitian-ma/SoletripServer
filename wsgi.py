485a7f2b99fe9336c0fbfed603104bd69daea55f
shitian <mstcq@126.com>
2015-11-22 22:16:01 +0800

    Update develop key

--- wsgi.py
+++ wsgi.py
@@ -8,12 +8,13 @@ from wsgiref import simple_server
 from app import app
 from cloud import engine
 
-APP_ID = os.environ['LC_APP_ID']
-MASTER_KEY = os.environ['LC_APP_MASTER_KEY']
+APP_ID = 'M0fpy60YKxsgnfHnwktJxyey'        #os.environ['LC_APP_ID']
+MASTER_KEY = 'mR9Q27uauBapHMrCe2j5L45x'    #os.environ['LC_APP_MASTER_KEY']
 PORT = int(os.environ['LC_APP_PORT'])
 
 
-leancloud.init(APP_ID, master_key=MASTER_KEY)
+#leancloud.init(APP_ID, master_key=MASTER_KEY)
+leancloud.init('M0fpy60YKxsgnfHnwktJxyey', master_key='mR9Q27uauBapHMrCe2j5L45x')
 
 application = engine
 
